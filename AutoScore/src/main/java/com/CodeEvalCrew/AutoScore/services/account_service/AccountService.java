612e1461fbc9f1b0517888d3723c4741388611c3
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2024-09-29 08:17:57 +0700

    Add structure of project

---
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/services/account_service/AccountService.java
@@ -0,0 +1,8 @@
+package com.CodeEvalCrew.AutoScore.services.account_service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService implements IAccountService {
+    
+}
