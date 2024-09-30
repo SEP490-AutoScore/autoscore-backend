062bbb149a0ae406a2ff8e8b45ddcc48e8344b61
Nhat Truong <nhattruong3314@gmail.com>
2024-09-30 18:25:01 +0700

    fix/updateStarter,commentSercurity

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/controllers/AccountController.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/controllers/AccountController.java
@@ -2,13 +2,16 @@ package com.CodeEvalCrew.AutoScore.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("api/account/")
 public class AccountController {
-    @GetMapping("api/v1/get")
-    public String getMethodName(@RequestParam String param) {
+    @GetMapping("testing/{param}")
+    public String getMethodName(@PathVariable String param) {
         return param;
     }
     
