d741074e3e6711250093fd1eb1c54c5b566a2a1a
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2024-10-07 17:57:07 +0700

    fix permisison controller

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/controllers/RoleController.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/controllers/RoleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,7 +105,7 @@ public class RoleController {
     }
 
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('DELETE_ROLE')")
-    @PostMapping("/delete/{roleId}")
+    @DeleteMapping("/delete/{roleId}")
     public ResponseEntity<?> deleteRole(@PathVariable Long roleId) {
         try {
             OperationStatus status = roleService.deleteRole(roleId);
