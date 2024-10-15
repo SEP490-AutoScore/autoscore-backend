8b8e89d10a96ae53f0fadaf3b2e206973e249262
Nhat Truong <nhattruong3314@gmail.com>
2024-10-15 13:10:13 +0700

    feat/merge-work-withdown-load-exam

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/AutoScoreApplication.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/AutoScoreApplication.java
@@ -1,16 +1,19 @@
 package com.CodeEvalCrew.AutoScore;
 
+import javax.xml.bind.JAXBException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.CodeEvalCrew.AutoScore")
 @EnableJpaRepositories(basePackages = "com.CodeEvalCrew.AutoScore.repositories")
 public class AutoScoreApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JAXBException {
 		SpringApplication.run(AutoScoreApplication.class, args);
 	}
 
