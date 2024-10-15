c2537343aa822a1103749b65501cc8cd518c6a92
Nhat Truong <nhattruong3314@gmail.com>
2024-10-15 13:15:11 +0700

    fix/pull-push

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/AutoScoreApplication.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/AutoScoreApplication.java
@@ -1,7 +1,5 @@
 package com.CodeEvalCrew.AutoScore;
 
-import javax.xml.bind.JAXBException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.CodeEvalCrew.AutoScore.repositories")
 public class AutoScoreApplication {
 
-	public static void main(String[] args) throws JAXBException {
+	public static void main(String[] args) {
 		SpringApplication.run(AutoScoreApplication.class, args);
 	}
 
