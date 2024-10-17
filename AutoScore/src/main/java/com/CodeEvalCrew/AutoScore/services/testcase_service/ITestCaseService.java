17834b51c56db5d9ca195bc91db72646996acbf9
votrongvuong672 <votrongvuong001@gmail.com>
2024-10-17 22:32:12 +0700

    fix examdatabase controller + testcase

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/services/testcase_service/ITestCaseService.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/services/testcase_service/ITestCaseService.java
@@ -1,5 +1,5 @@
 package com.CodeEvalCrew.AutoScore.services.testcase_service;
 
 public interface ITestCaseService {
-    String getAIResponse(Long examDatabaseId, Long examBaremId, int minimumNumberOfTestcases);
+    String getAIResponse(Long examDatabaseId);
 }
