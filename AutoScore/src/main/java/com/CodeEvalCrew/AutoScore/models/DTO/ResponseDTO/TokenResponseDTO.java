c3156222ce270e5ca5fb8db24cfd4be0190f56ff
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2024-11-20 03:40:17 +0700

    update auth

---
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/models/DTO/ResponseDTO/TokenResponseDTO.java
@@ -0,0 +1,14 @@
+package com.CodeEvalCrew.AutoScore.models.DTO.ResponseDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponseDTO {
+    private String accessToken;
+    private String refreshToken;
+    private long exp;
+}
