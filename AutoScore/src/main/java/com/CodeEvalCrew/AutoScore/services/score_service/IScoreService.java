2ba34797c62cd3b056eb046532b52c6ecfdcd4c8
VanMinhtp <vanminh101202@gmail.com>
2024-11-24 06:54:42 +0700

    getPlagiarism

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/services/score_service/IScoreService.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/services/score_service/IScoreService.java
@@ -3,6 +3,8 @@ package com.CodeEvalCrew.AutoScore.services.score_service;
 import java.io.IOException;
 import java.util.List;
 
+import com.CodeEvalCrew.AutoScore.models.DTO.ResponseDTO.CodePlagiarismResponseDTO;
+import com.CodeEvalCrew.AutoScore.models.DTO.ResponseDTO.ScoreDetailsResponseDTO;
 import com.CodeEvalCrew.AutoScore.models.DTO.ResponseDTO.ScoreOverViewResponseDTO;
 import com.CodeEvalCrew.AutoScore.models.DTO.ResponseDTO.ScoreResponseDTO;
 
@@ -15,4 +17,8 @@ public interface IScoreService {
     void exportScoresToExcel(HttpServletResponse response, List<ScoreResponseDTO> scores) throws IOException;
 
     List<ScoreOverViewResponseDTO> getScoreOverView();
+
+    List<ScoreDetailsResponseDTO> getScoreDetailsByScoreId(Long scoreId);
+
+    List<CodePlagiarismResponseDTO> getCodePlagiarismByScoreId(Long scoreId);
 }
