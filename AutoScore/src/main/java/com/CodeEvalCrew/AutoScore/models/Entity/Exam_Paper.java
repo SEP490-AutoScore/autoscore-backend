394a02580968aca53d6cfaa1ce7d1e94ef3cd5c4
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2024-11-24 19:23:31 +0700

    update upload student source

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/models/Entity/Exam_Paper.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/models/Entity/Exam_Paper.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,6 +37,7 @@ public class Exam_Paper {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long examPaperId;
     private String examPaperCode;
+    @Enumerated(EnumType.STRING)
     private Exam_Status_Enum status;
     private String instruction;
     @Column(columnDefinition = "int default 90")
